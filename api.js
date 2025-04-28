7b96c617a7ac42d0e2dc74162c66637b215a2bf0
Djamchid <djamchid@gmail.com>
2025-04-28 16:38:43 +0200

    Update api.js

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
 // Configuration
-const API_URL = 'https://script.google.com/macros/s/VOTRE_ID_DE_DEPLOIEMENT/exec';
+const API_URL = 'https://script.google.com/macros/s/https://script.google.com/macros/s/AKfycbwqBVhPQOByfcYSuY22t-NFC1vFdjF7nqdImr_Iy6SxDSNE0LC9Iqe5ud0HPKPhdMwAEw/exec/';
 const ALLOWED_ORIGINS = ['https://djamchid.github.io'];
 const MAX_PAYLOAD_SIZE = 1024 * 1024; // 1MB
 
